cac5b5fcf42e1e74bed276e32a52cf126e681b9b
Thiago Bortoletto <thiago.bortoletto@dextra-sw.com>
2016-10-25 17:39:58 -0200

    asd

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Pessoal de devops</h2>
+          <h2>Pessoal de devopss</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
